37ff5ad9c6a8f9de08ba6202aad7fd44cfd0bb04
AzzaElsayad <azzasobhy8@gmail.com>
2022-12-21 12:19:18 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-
-int main (void)
 /**
-*cod block
-*return(0)
-*/
+ * *main - code block
+ * *return: 0
+ * */
+
+int main(void)
 {
-   	puts("\"Programming is like building a multilingual puzzle");
-  	return(0);
-} 
+		puts("\"Programming is like building a multilingual puzzle");
+	      	return (0);
+}

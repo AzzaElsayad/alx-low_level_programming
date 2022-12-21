ff1694c0080098b324ba39f8be1dca2eb2522668
AzzaElsayad <azzasobhy8@gmail.com>
2022-12-21 11:59:17 -0800

    1-2-3-4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//include library stdio
+int main (void)
+/*
+*cod block
+*return 0
+*/
+{
+   puts("\"Programming is like building a multilingual puzzle");
+   return(0);
+  } 

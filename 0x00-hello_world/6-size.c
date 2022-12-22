b2cb1ff32e292afbe0fe436d307cbf4da58021b1
AzzaElsayad <azzasobhy8@gmail.com>
2022-12-22 05:56:27 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int i ;
+	  char c;
+	    float f;
+	      long int l;
+	        long long int ll;
+		  printf("size of a int : %lu bytes\n",sizeof(i));
+		    printf("size of a char : %lu\n",sizeof(c));
+		      printf("size of a float : %lu byts\n",sizeof(f));
+		        printf("size of a long int : %lu byts\n",sizeof(l));
+			  printf("size of a long long int : %lu byts\n",sizeof(ll));
+	return (0);
+}

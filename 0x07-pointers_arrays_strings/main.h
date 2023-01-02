279eaaf91e80b419ea92ddef61b786c193042dda
AzzaElsayad <azzasobhy8@gmail.com>
2023-01-02 15:49:29 -0800

    0memset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,61 +1,25 @@
-lf(char *str);                                              
+#ifndef MAIN_H
 
-                                                                        
+#define MAIN_H
 
-void print_array(int *a, int n);                                        
+int _putchar(char c);
 
-                                                                        
+char *_memset(char *s, char b, unsigned int n);
 
-char *_strcpy(char *dest, char *src);                                   
+char *_memcpy(char *dest, char *src, unsigned int n);
 
-                                                                        
+char *_strchr(char *s, char c);
 
-int _atoi(char *s);                                                     
+unsigned int _strspn(char *s, char *accept);
 
-                                                                        
+char *_strpbrk(char *s, char *accept);
 
-char *_strcat(char *dest, char *src);                                   
+char *_strstr(char *haystack, char *needle);
 
-                                                                        
+void print_chessboard(char (*a)[8]);
 
-char *_strncat(char *dest, char *src, int n);                           
+void print_diagsums(int *a, int size);
 
-                                                                        
+void set_string(char **s, char *to);
 
-char *_strncpy(char *dest, char *src, int n);                           
-
-                                                                        
-
-int _strcmp(char *s1, char *s2);                                        
-
-                                                                        
-
-void reverse_array(int *a, int n);                                      
-
-                                                                        
-
-char *string_toupper(char *);                                           
-
-                                                                        
-
-char *cap_string(char *);                                               
-
-                                                                        
-
-char *leet(char *);                                                     
-
-                                                                        
-
-char *rot13(char *);                                                    
-
-                                                                        
-
-void print_number(int n);                                               
-
-                                                                        
-
-char *infinite_add(char *n1, char *n2, char *r, int size_r);            
-
-                                                                        
-
-void print_buffer(char *b, int size);
+#endif;

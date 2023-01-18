df5cfc19af1c9809a2b9ea1980cb374523b345b5
AzzaElsayad <117826810+AzzaElsayad@users.noreply.github.com>
2023-01-18 22:37:14 +0200

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* print the last digit */
+/* main - Determine if the last digit of a random number is
+  greater than or less than 5, or is zero */
 int main(void)
 {
 	int n;
@@ -11,17 +10,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-  if (n%10 > 5 )
-  {
-    printf("Last digit of %d is %d and is greater than 5\n", n, n%10);
-    }
-  else if (n%10 == 0)
-  {
-    printf("Last digit of %d is %d and is zero\n", n, n%10);
+  if (n % 10 > 5 )
+ {
+  printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+ }
+  else if (n % 10 == 0)
+ {
+  printf("Last digit of %d is %d and is zero\n", n, n % 10);
+ }
+ else
+ {
+  printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
   }
-  else
-  {
-    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n%10);
-  }
-	return (0);
+return (0);
 }

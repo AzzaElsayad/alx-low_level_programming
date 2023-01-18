759337e1b94fe6dab495a3c358c1e39b8808e2b5
AzzaElsayad <117826810+AzzaElsayad@users.noreply.github.com>
2023-01-18 23:27:10 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,14 @@
-#includ
+#include <stdio.h>
+/**
+* main - print alphabet
+return 0
+*/
+int main(void) {
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+        putchar(letter);
+    putchar('\n');
+
+    return 0;
+}

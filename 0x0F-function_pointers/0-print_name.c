9d5cfc9a54feb6e5c55af120a288685497524afe
AzzaElsayad <117826810+AzzaElsayad@users.noreply.github.com>
2023-01-21 20:44:03 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
-reas
+#include "main.h"
+/**
+ * main - Print "_putchar" followed by a new line.
+ * Description: You are not allowed to include standard libraries.
+ * Return: 0
+ */
+int main(void)
+{
+	void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
+}
